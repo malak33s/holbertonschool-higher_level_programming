708e0ce80015f9949ff4bb436b85ae052ab4eba6
malak33s <melaksarhani1bga@gmail.com>
2024-05-19 21:09:00 +0000

    task one

---
+++ python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Define a class """
+class Square:
+    """create a Square class"""
+    def __init__(self, size):
+        """ Init a new square and this  size"""
+        self.__size = size
