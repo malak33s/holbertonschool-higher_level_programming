bd1234f9bc2e5e669bb8cdff124506c3a6d7be61
malak33s <melaksarhani1bga@gmail.com>
2024-05-23 17:38:20 +0000

    add two  paramets miss for my file testing

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+"""Defines two integers for the addition function"""
+
 
 def add_integer(a, b=98):
-    if not isinstance(a, (int, float)):
+    """Returns a + b
+
+    a and b must be int or float
+
+    Raises:
+        TypeError: if a or b is a non-integers and non-floats
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
 
-    return int(a) + int(b)
+    return (int(a) + int(b))
