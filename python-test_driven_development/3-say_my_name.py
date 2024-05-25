209515422dcd0aeb3c5d4480148477911452c9af
malak33s <melaksarhani1bga@gmail.com>
2024-05-25 13:49:57 +0000

    re add task 3 bc remove

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""function that print my name is <first name> <last name>"""
+
+
+def say_my_name(first_name, last_name=""):
+    """print my name is <first name> <last name>
+
+    first_name must be strings otherwise
+    last_name must be strings otherwise
+
+    TypeError: first_name must be a string or last_name must be a string.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
