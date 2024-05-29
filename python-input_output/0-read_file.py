9644a0a235cbe4b0ac4ce79a8bb35036352eb962
malak33s <melaksarhani1bga@gmail.com>
2024-05-29 12:26:38 +0000

    add comment

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,10 @@ function read a text file and print its contents.
 """
 
 def read_file(filename=""):
+    """ read a text file and print
+    args:
+    file str): name of file.
+    """
     with open(filename, 'r', encoding='utf-8') as f:
         print(f.read(), end='')
 
