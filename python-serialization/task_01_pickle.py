991bb24d4858fbca254f61e3635e813c1212eb20
malak33s <melaksarhani1bga@gmail.com>
2024-05-30 18:49:05 +0000

    testing code

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,28 @@
+import pickle
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Is Student:", self.is_student)
+
+    def serialize(self, filename):
+        try:
+            with open(filename, 'wb') as f:
+                pickle.dump(self, f)
+        except (pickle.PickleError, IOError):
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, 'rb') as f:
+                return pickle.load(f)
+        except (pickle.PickleError, IOError, FileNotFoundError):
+            return None
+
