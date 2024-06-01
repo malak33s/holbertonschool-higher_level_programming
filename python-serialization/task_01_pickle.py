a4893675e1f6bd1021f2d12bc60e1327662d83d4
malak33s <melaksarhani1bga@gmail.com>
2024-06-01 16:03:55 +0000

    task one coorection

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,34 +1,41 @@
-#!/usr/bin/env python3
-"""Learn pickle module"""
+#!/usr/bin/python3
+"""Define serialization and deserialization pickle"""
 import pickle
 
 
 class CustomObject:
+    """Define class CustomObject"""
     def __init__(self, name, age, is_student):
-        """Initialize a CustomObject"""
+        """"Initialize CustomObject
+
+        Args:
+            name (str): name of the object
+            age (int): age of the object
+            is_student (bool): is the object a student
+        """
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
-        """Display CustomObject"""
+        """Define methode to print object's attributes"""
         print("Name: {}".format(self.name))
         print("Age: {}".format(self.age))
-        print("Is Student: {}".format(self.is_student))
+        print("Is student: {}".format(self.is_student))
 
     def serialize(self, filename):
-        """Serialize CustomObject"""
+        """Define method to serialize object"""
         try:
             with open(filename, 'wb') as f:
                 pickle.dump(self, f)
-        except (FileNotFoundError, pickle.PickleError):
+        except (FileNotFoundError, Exception):
             return None
 
     @classmethod
     def deserialize(cls, filename):
-        """Deserialize CustomObject"""
+        """Define methode to deserialize object"""
         try:
             with open(filename, 'rb') as f:
                 return pickle.load(f)
-        except (FileNotFoundError, pickle.PickleError):
+        except (FileNotFoundError, Exception):
             return None
