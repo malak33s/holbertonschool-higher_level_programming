ee5e6e85040304e6d63f9082d042ecf5a90110ce
malak33s <melaksarhani1bga@gmail.com>
2024-09-03 19:55:40 +0200

    create new sentence output not complete

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ langu that combines remarkable power with very clear python"
+print(f"{str[39:61]} with {str[-7:]}")
