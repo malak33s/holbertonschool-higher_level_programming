319f5c2c37940c5eaf3cd2e9e1d9e586f4c0a3d9
malak33s <melaksarhani1bga@gmail.com>
2024-09-06 09:39:35 +0200

    update

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for letter_code in range(ord('a'), ord('z')+1):
     letter = chr(letter_code)
-    if letter not in "qe":
+    if letter != "e" and letter != "q":
         print("{}".format(letter), end='')
         
