5af795604a4f5ce217bc991665a1a0f58eead679
malak33s <melaksarhani1bga@gmail.com>
2024-09-11 21:58:13 +0200

    task 0 updte correction

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     """Function that prints all integers in a list."""
     for number in my_list:
-        print("{:d}".format(number))  # {:d} spécifie que number est un entier
+        print("{:d}".format(number))
