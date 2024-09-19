8aa0a256bca3837706b658415f6cf0b6eb59c192
malak33s <melaksarhani1bga@gmail.com>
2024-09-19 05:04:55 +0200

    correction pycodestyle

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a class """
+
+
+class Square:
+    """create a Square class"""
+
+    def __init__(self, size=0):
+        """ Init a new square and this  size"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
