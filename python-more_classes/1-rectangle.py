974a1a9686dd26147c1be54cfb275adad209e2d7
malak33s <melaksarhani1bga@gmail.com>
2024-09-19 18:04:58 +0200

    task 1 update

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Defines a rectangles"""
+
+
+class Rectangle:
+    """Define rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a rectangle."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """set width of rectange"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """set height of rectange"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
