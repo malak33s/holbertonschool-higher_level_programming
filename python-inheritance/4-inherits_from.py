fbd9803d348a6f4b50ff2581f8788f6b4866bcef
malak33s <melaksarhani1bga@gmail.com>
2024-09-24 18:17:11 +0200

    correction documentation pycode style and doc modules

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module 4-inherits_from
+Defines a function that checks if an object is an instance of a class that
+inherited (directly or indirectly) from the specified class.
+"""
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class; otherwise False.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        bool: True if obj is an instance of a class that inherited from a_class,
+              but not an instance of a_class itself.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Module 5-base_geometry
+Defines an empty class BaseGeometry
+"""
+
+class BaseGeometry:
+    """An empty class"""
+    pass
