de561fce02060e16ad71bd24113434c41e35dde1
malak33s <melaksarhani1bga@gmail.com>
2024-09-24 17:48:01 +0200

    documention with gpt

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-
+"""
+Module 1-my_list
+Defines a class MyList that inherits from list
+"""
 
 class MyList(list):
-
+    """
+    A subclass of list that includes a method to print the list in sorted order.
+    """
     def print_sorted(self):
-
+        """
+        Prints the list sorted in ascending order.
+        Does not modify the original list.
+        """
         print(sorted(self))
