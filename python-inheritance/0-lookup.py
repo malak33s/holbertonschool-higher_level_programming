63ab30fe701c9877bd8a23a97548950564775761
malak33s <melaksarhani1bga@gmail.com>
2024-09-24 15:59:11 +0200

    pycodestyle

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,13 +2,5 @@
 
 
 def lookup(obj):
-    """
-    Returns the list of available attributes and methods of an object
-    
-    Args:
-        obj: The object to inspect
-    
-    Returns:
-        A list of the available attributes and methods of the object
-    """
+
     return dir(obj)
--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,7 +2,7 @@
 
 
 class MyList(list):
-    
+
     def print_sorted(self):
-      
+
         print(sorted(self))
