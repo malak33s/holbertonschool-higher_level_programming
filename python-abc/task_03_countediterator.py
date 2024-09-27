c5f8cf05cdefb8f5b6e7aaa71e981994a5e350bf
malak33s <melaksarhani1bga@gmail.com>
2024-09-27 12:25:07 +0200

    task 1

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,13 @@
+
+class CountedIterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)  # Initialize the iterator from the iterable
+        self.count = 0  # Counter for items iterated
+
+    def __next__(self):
+        # Increment the count and get the next item
+        self.count += 1
+        return next(self.iterator)  # Fetch the next item
+
+    def get_count(self):
+        return self.count  # Return the count of items iterated
