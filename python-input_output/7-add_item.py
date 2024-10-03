197804c7f704a12dcc6b52450c437fe09fa72c36
malak33s <melaksarhani1bga@gmail.com>
2024-10-03 15:44:50 +0200

    commentaires declaration

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""Script that adds all arguments to a Python list and saves them to a JSON file."""
+"""that add all arguments to a Python list & save them to a JSON file."""
 
 import sys
 import json
+
 
 def save_to_json_file(my_obj, filename):
     """Writes an Object to a text file, using a JSON representation."""
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(my_obj, f)
 
+
 def load_from_json_file(filename):
     """Creates an Object from a 'JSON file'."""
     with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
 
+
 if __name__ == "__main__":
     filename = "add_item.json"
-    
+
     # Load existing items from the JSON file if it exists
     try:
         items = load_from_json_file(filename)
