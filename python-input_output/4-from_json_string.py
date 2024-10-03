13532e0edc7aeb9c4cac8076accd38ffd98d71c7
malak33s <melaksarhani1bga@gmail.com>
2024-10-03 15:36:38 +0200

    missing blank line 6

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""Defines a function to convert a JSON string to a Python object."""
+
 import json
 
 
 def from_json_string(my_str):
-    """Returns an object (Python data structure) represented by a JSON string."""
+    """Returns an object represented by a JSON string.
+
+    Args:
+        my_str: The JSON string to be converted.
+
+    Returns:
+        The Python object represented by the JSON string.
+    """
     return json.loads(my_str)
