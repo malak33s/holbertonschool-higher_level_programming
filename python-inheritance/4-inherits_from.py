faa0524577d0452ce536d4f8e24146d18ea1be44
malak33s <melaksarhani1bga@gmail.com>
2024-10-03 17:43:44 +0200

    rattrapage pycoe ancienne task

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,6 +5,7 @@ Defines a function that checks if an object is an instance of a class that
 inherited (directly or indirectly) from the specified class.
 """
 
+
 def inherits_from(obj, a_class):
     """
     Returns True if the object is an instance of a class that inherited
@@ -15,7 +16,7 @@ def inherits_from(obj, a_class):
         a_class: The class to compare against.
 
     Returns:
-        bool: True if obj is an instance of a class that inherited from a_class,
+        bool: True if obj is an instance,
               but not an instance of a_class itself.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
