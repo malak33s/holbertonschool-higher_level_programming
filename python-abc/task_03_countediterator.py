3759defc930b6ec723a256720dde6225703a8071
malak33s <melaksarhani1bga@gmail.com>
2024-10-03 15:55:02 +0200

    rattrapage de task

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,14 +1,28 @@
+#!/usr/bin/python3
+"""
+Create an iter class that extends the Python list class
+"""
+
 
 class CountedIterator:
-    def __init__(self, iterable):
-        self.iterator = iter(iterable)  # Initialize the iterator from the iterable
-        self.count = 0  # Counter for items iterated
+    """an iter class CountedIterator"""
+
+    def __init__(self, data):
+        self.iterator = iter(data)  # iter() pour créer un itérateur
+        self.count = 0
+
+    def __iter__(self):
+        return self  # Return the iterator object itself
 
     def __next__(self):
-        # Increment the count and get the next item
-        self.count += 1
-        return next(self.iterator)  # Fetch the next item
+        # si une exception est levée par next(self.iterator)
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            # le bloc de code qui suit cette ligne sera exécuté.
+            raise StopIteration
 
     def get_count(self):
-        return self.count  # Return the count of items iterated
-    
+        return self.count
