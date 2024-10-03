35d461f3f035fd1eaaf1df651fe5883fa9211261
malak33s <melaksarhani1bga@gmail.com>
2024-10-03 14:45:49 +0200

    retest

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Defines a text file-writing function"""
+
 def write_file(filename="", text=""):
-    """Writes a string to a text file and returns the number of characters written"""
-    with open(filename, 'w', encoding='utf-8') as f:
-        return f.write(text)
+    """Writes a string to a text file and returns the number of characters written."""
+    
+    # Ouvre le fichier en mode écriture ('w'), ce qui crée le fichier s'il n'existe pas
+    # ou écrase son contenu s'il existe déjà
+    with open(filename, 'w', encoding="utf-8") as f:
+        i = f.write(text)  # Écrit le texte dans le fichier et stocke le nombre de caractères écrits
+    return i  # Retourne le nombre de caractères écrits
