f319b35afd6a55a86e495b69c75ac0044b00a919
malak33s <you@example.com>
2024-10-11 15:13:05 +0000

    task2

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,25 @@
+import requests
+import csv
+
+# Récupère  enregistre les posts dans un CSV
+def fetch_and_save_posts():
+    r = requests.get("https://jsonplaceholder.typicode.com/posts")
+    
+    if r.status_code == 200:
+        posts = r.json()
+        with open('posts.csv', 'w', newline='') as f:
+            # Ajout de 'userId' dans les fieldnames
+            writer = csv.DictWriter(f, fieldnames=["userId", "id", "title", "body"])
+            writer.writeheader()  # En-tête CSV
+            writer.writerows(posts)  # Écrit les posts
+import requests
+
+# Récupère et affiche les posts
+def fetch_and_print_posts():
+    r = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print(f"Status Code: {r.status_code}")
+
+    if r.status_code == 200:
+        posts = r.json()
+        for post in posts:
+            print(post['title'])
