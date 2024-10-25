aefcc2312eac66f95a43131c6b7e61e474c327ca
malak33s <melaksarhani1bga@gmail.com>
2024-10-25 13:54:42 +0000

    update not correct

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- Script to list all tables in the current database
+SHOW TABLES;
+
---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Script to create table first_table with columns id and name
+CREATE TABLE IF NOT EXISTS first_table (
+  id INT,
+  name VARCHAR(256)
+);
+
