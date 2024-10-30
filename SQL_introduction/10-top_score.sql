144c878eb3691047acd6e8e787e4005a17d91bbf
malak33s <melaksarhani1bga@gmail.com>
2024-10-30 17:10:25 +0100

    commentaire not found

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,4 @@
-SELECT score, name
-FROM second_table
+-- Lister tous les enregistrements de second_table, triés par score
+SELECT score, name 
+FROM second_table 
 ORDER BY score DESC;
--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,5 @@
-SELECT score, name
-FROM second_table
-WHERE score >= 10
+-- Lister les enregistrements avec un score >= 10, triés par score
+SELECT score, name 
+FROM second_table 
+WHERE score >= 10 
 ORDER BY score DESC;
