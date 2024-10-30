36bc35d8efa9414655ff60ae9680cda5afffc10f
malak33s <melaksarhani1bga@gmail.com>
2024-10-30 16:54:15 +0100

    return in vs + changment mdp sudo

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,14 +1,12 @@
--- Crée second_table avec les colonnes id, name, score si elle n'existe pas déjà
+-- Create second_table with required fields
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
 
--- Insère plusieurs lignes dans second_table
-INSERT INTO second_table (id, name, score) VALUES
-(1, 'John', 10),
-(2, 'Alex', 3),
-(3, 'Bob', 14),
-(4, 'George', 8);
-
+-- Insert required rows
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
