c220d93d1726699cb9e15b4b72be703c32d41b79
malak33s <melaksarhani1bga@gmail.com>
2024-11-02 17:34:17 +0100

    task two

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Script qui crée la base de données hbtn_0d_2 et l'utilisateur user_0d_2 avec le privilège SELECT
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
