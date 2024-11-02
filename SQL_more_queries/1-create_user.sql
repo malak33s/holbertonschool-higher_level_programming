56605cc5a8eba3eb5b68a2f19a14552ad3cf2702
malak33s <melaksarhani1bga@gmail.com>
2024-11-02 17:27:48 +0100

    task 1 coorection

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Script qui crée l'utilisateur user_0d_1 avec tous les privilèges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
